9bd963cde6d4c8d0b98721103d6e28a119d48707
oxullo <x@brainrapers.org>
2008-06-05 15:59:10 +0000

    Sponc: added copyright information and wiki reference
    svn path=/trunk/avg_media/mtc/sponc/; revision=2899

--- Audio.py
+++ Audio.py
@@ -1,3 +1,26 @@
+# Copyright (C) 2007
+#    Martin Heistermann, <mh at lulzmeister dot de>
+#    Tim Grocki, <drogenkonsument at gmail dot com>
+#
+# This file is part of sponc.
+#
+# sponc is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# sponc is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with sponc.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Audio gateway by OXullo Intersecans <x@xul.it>
+# Setup howto can be found here:
+# https://www.libavg.de/wiki/index.php/Audio_on_Sponc
+
 g_AudioEnabled = True
 try:
     import scsynth
