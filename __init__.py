a9ea05a10eec16901b35b9df7e6398800226c7b3
mh <mh@libavg.de>
2009-04-30 15:44:59 +0000

    sponc: create preview node, removed empty *.avg and unused avgtrackerrc
    svn path=/trunk/avg_media/mtc/sponc/; revision=3786

--- __init__.py
+++ __init__.py
@@ -1,4 +1,15 @@
-from Game import Game, disableExitButton
+import os
 
-apps = ({'class': Game},)
+from Game import Game, disableExitButton
+from libavg.AVGAppUtil import getMediaDir, createImagePreviewNode
+
+__all__ = [ 'apps', 'disableExitButton',]
+
+def createPreviewNode(maxSize):
+    filename = os.path.join(getMediaDir(__file__), 'preview.png')
+    return createImagePreviewNode(maxSize, absHref = filename)
+
+apps = ({'class': Game,
+            'createPreviewNode': createPreviewNode},
+            )
 
