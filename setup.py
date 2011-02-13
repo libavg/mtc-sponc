1ce68709cbdedd1d57d74cd6eedc08f68d957510
uzadow <uzadow@libavg.de>
2011-02-13 12:29:01 +0000

    avg_media/mtc/sponc: Added setup.py.
    svn path=/trunk/avg_media/mtc/sponc/; revision=5560

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+    name='Sponc',
+    version='1.0',
+    author='Martin Heisterman',
+    author_email='mh@lulzmeister.de',
+    url='http://www.sponc.de/',
+    license='GPL',
+    packages=['sponc'],
+    scripts=['scripts/sponc'],
+    package_data={
+            'sponc': ['media/*.png', 'media/Sound/*.wav', 'media/Sound/scsyndefs/*',
+                    'fonts/*.ttf'],
+    }
+)
+
